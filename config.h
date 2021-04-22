f03f1d0b77fe2ee44a5181a7eb8f64c65bb4a0f6
Pontus Laos <laos.pontus@gmail.com>
2021-04-22 16:42:03 +0200

    Removed dumb click action

--- config.h
+++ config.h
@@ -179,7 +179,6 @@ static Key keys[] = {
 static Button buttons[] = {
     /* click                event mask      button          function        argument */
     { ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-    { ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
     { ClkWinTitle,          0,              Button2,        zoom,           {0} },
     { ClkStatusText,        0,              Button1,        sigdwmblocks,   {.i = 1} },
     { ClkStatusText,        0,              Button2,        sigdwmblocks,   {.i = 2} },
